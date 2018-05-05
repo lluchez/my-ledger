c772a4d14f8b384e0b371d7aabaa40e9332295ba
Lionel Luchez <lionel.luchez@gmail.com>
2018-05-05 00:36:03 -0500

    Spinner and fast loading

--- webpack/index.js
+++ webpack/index.js
@@ -7,4 +7,7 @@ document.addEventListener("DOMContentLoaded", function() {
   console.log("in DOMContentLoaded")
   const app = document.getElementById("app"), props = JSON.parse(app.getAttribute("react-props"))
   ReactDOM.render(React.createElement(App, props), app)
+
+  const spinner = document.getElementById('app-loader')
+  spinner.className = 'loaded'
 })
